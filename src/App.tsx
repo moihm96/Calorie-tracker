ad64f06f7f66d41026e7f0bfb0cb068653551b13
Moises Heredia Molina <moises.molina@karnovgroup.com>
2024-04-26 14:08:39 +0100

    Listing activities

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { useReducer } from "react";
 import Form from "./components/Form";
 import Header from "./components/Header";
 import { activityReducer, initialState } from "./reducers/activityReducer";
+import ActivityList from "./components/ActivityList";
 
 function App() {
-  const [, dispatch] = useReducer(activityReducer, initialState);
+  const [state, dispatch] = useReducer(activityReducer, initialState);
 
   return (
     <>
@@ -13,6 +14,9 @@ function App() {
         <div className="max-w-4xl mx-auto">
           <Form dispatch={dispatch} />
         </div>
+      </section>
+      <section className="p-10 mx-auto max-w-4xl">
+        <ActivityList activities={state.activities} />
       </section>
     </>
   );
