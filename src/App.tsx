dfe43381f4b323b122b80ee8dc57b0ee58e27f6f
Moises Heredia Molina <moises.molina@karnovgroup.com>
2024-04-29 10:47:22 +0100

    Calories dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Form from "./components/Form";
 import Header from "./components/Header";
 import { activityReducer, initialState } from "./reducers/activityReducer";
 import ActivityList from "./components/ActivityList";
+import CalorieTracker from "./components/CalorieTracker";
 
 function App() {
   const [state, dispatch] = useReducer(activityReducer, initialState);
@@ -16,6 +17,11 @@ function App() {
       <section className="bg-lime-500 py-20 px-5">
         <div className="max-w-4xl mx-auto">
           <Form dispatch={dispatch} state={state} />
+        </div>
+      </section>
+      <section className="bg-gray-800 py-10">
+        <div className="max-w-4xl mx-auto">
+          <CalorieTracker activities={state.activities} />
         </div>
       </section>
       <section className="p-10 mx-auto max-w-4xl">
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ export default function Header({ state, dispatch }: HeaderProps) {
   return (
     <>
       <header className="bg-lime-600 py-3">
-        <div className="max-w-4xl mx-auto flex justify-between">
+        <div className="max-w-4xl mx-auto flex justify-between items-center">
           <h1 className="text-center text-lg font-bold text-white uppercase">
             Calorie tracker
           </h1>
