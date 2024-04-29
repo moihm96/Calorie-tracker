5c475aedf2c56e56545e7d8d0b31d518bc57158e
Moises Heredia Molina <moises.molina@karnovgroup.com>
2024-04-29 10:29:51 +0100

    Reiniciar App button

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <>
-      <Header />
+      <Header dispatch={dispatch} state={state} />
       <section className="bg-lime-500 py-20 px-5">
         <div className="max-w-4xl mx-auto">
           <Form dispatch={dispatch} state={state} />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,16 @@
-export default function Header() {
+import { Dispatch, useMemo } from "react";
+
+import { ActivityActions, ActivityState } from "../reducers/activityReducer";
+
+type HeaderProps = {
+  dispatch: Dispatch<ActivityActions>;
+  state: ActivityState;
+};
+export default function Header({ state, dispatch }: HeaderProps) {
+  const canRestartApp = useMemo(
+    () => state.activities.length,
+    [state.activities]
+  );
   return (
     <>
       <header className="bg-lime-600 py-3">
@@ -6,6 +18,18 @@ export default function Header() {
           <h1 className="text-center text-lg font-bold text-white uppercase">
             Calorie tracker
           </h1>
+
+          <button
+            className="bg-gray-800 hover:bg-gray-900 p-2 font-bold uppercase text-white cursor-pointer rounded-lg text-sm disabled:opacity-10"
+            disabled={!canRestartApp}
+            onClick={() =>
+              dispatch({
+                type: "restart-app",
+              })
+            }
+          >
+            Reiniciar App
+          </button>
         </div>
       </header>
     </>
